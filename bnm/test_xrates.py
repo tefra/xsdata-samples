ba383fad64afe6ec2759f5a1154fe821e5c813e9
Chris <chris@komposta.net>
2020-04-20 17:55:57 +0300

    National bank of Moldova xrates I think :)

---
+++ bnm/test_xrates.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from unittest import mock
+from unittest import TestCase
+
+from requests import Response
+
+from bnm.xrates import Currencies
+from bnm.xrates import Currency
+from bnm.xrates import fetch
+
+
+class FetchTest(TestCase):
+    @mock.patch("bnm.xrates.requests.get")
+    def test_parser(self, mock_requests_get):
+        response = Response()
+        response.status_code = 200
+
+        sample_xml = Path(__file__).with_name("sample.xml").read_text()
+        type(response).text = mock.PropertyMock(return_value=sample_xml)
+        mock_requests_get.return_value = response
+        currencies = fetch()
+
+        self.assertEqual("Official exchange rate", currencies.name)
+        self.assertEqual("19.04.2020", currencies.date)
+        self.assertEqual(42, len(currencies.values))
+        self.assertIsInstance(currencies, Currencies)
+        self.assertIsInstance(currencies.values[0], Currency)
---
+++ bnm/xrates.py
@@ -0,0 +1,51 @@
+import datetime
+from dataclasses import asdict
+from dataclasses import dataclass
+from dataclasses import field
+from decimal import Decimal
+from typing import List
+
+import requests
+from xsdata.formats.dataclass.parsers import XmlParser
+
+
+"""https://bnm.md/en/content/official-exchange-rates"""
+
+
+@dataclass
+class Currency:
+    id: int = field(metadata=dict(type="Attribute", name="ID"))
+    name: str = field(metadata=dict(type="Attribute", name="Name"))
+    num_code: int = field(metadata=dict(type="Element", name="NumCode"))
+    iso_code: str = field(metadata=dict(type="Element", name="CharCode"))
+    nominal: int = field(metadata=dict(type="Element", name="Nominal"))
+    value: Decimal = field(metadata=dict(type="Element", name="Value"))
+
+
+@dataclass
+class Currencies:
+    class Meta:
+        name = "ValCurs"
+
+    date: str = field(metadata=dict(type="Attribute", name="Date"))
+    name: str = field(metadata=dict(type="Attribute"))
+    values: List[Currency] = field(
+        default_factory=list, metadata=dict(type="Element", name="Valute")
+    )
+
+
+def fetch():
+    now = datetime.datetime.now()
+    today = now.strftime("%d.%m.%Y")
+    url = f"https://bnm.md/en/official_exchange_rates?get_xml=1&date={today}"
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    return XmlParser().from_string(response.text, Currencies)
+
+
+if __name__ == "__main__":
+    from pprint import pprint
+
+    pprint(asdict(fetch()))
