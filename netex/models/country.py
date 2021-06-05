1cc59ce86163bb4774c4721c9b7fcc4145cde195
Chris <chris@komposta.net>
2021-06-05 17:18:12 +0300

    Relax attribute override validations completely

--- netex/models/country.py
+++ netex/models/country.py
@@ -1,7 +1,5 @@
-from dataclasses import dataclass, field
-from typing import Optional
+from dataclasses import dataclass
 from netex.models.country_version_structure import CountryVersionStructure
-from netex.models.iana_country_tld_enumeration import IanaCountryTldEnumeration
 
 __NAMESPACE__ = "http://www.netex.org.uk/netex"
 
@@ -10,12 +8,3 @@ __NAMESPACE__ = "http://www.netex.org.uk/netex"
 class Country(CountryVersionStructure):
     class Meta:
         namespace = "http://www.netex.org.uk/netex"
-
-    id: Optional[IanaCountryTldEnumeration] = field(
-        default=None,
-        metadata={
-            "type": "Attribute",
-            "min_length": 2,
-            "max_length": 2,
-        }
-    )
