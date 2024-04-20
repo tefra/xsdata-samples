34890666bcabd6beefa738dc0f6419ee03e9e8c3
Christodoulos Tsoulloftas <chris@komposta.net>
2024-04-20 17:15:14 +0300

    Update netex performance script

--- netex/performance.py
+++ netex/performance.py
@@ -3,14 +3,13 @@ import time
 from contextlib import contextmanager
 from pathlib import Path
 from typing import Any
+from sys import getsizeof
 
 from xsdata.formats.dataclass.context import XmlContext
 from xsdata.formats.dataclass.parsers import XmlParser
 from xsdata.formats.dataclass.parsers.config import ParserConfig
 from xsdata.formats.dataclass.parsers.handlers import LxmlEventHandler
 from xsdata.formats.dataclass.parsers.handlers import XmlEventHandler
-
-from netex.models import PublicationDelivery
 
 
 @contextmanager
@@ -20,6 +19,25 @@ def timing(description: str) -> Any:
     ellapsed_time = time.time() - start
 
     print(f"{description}: {ellapsed_time}")
+
+
+def get_size(obj, seen=None):
+    """Recursively finds size of objects."""
+    size = getsizeof(obj)
+    if seen is None:
+        seen = set()
+    obj_id = id(obj)
+    if obj_id in seen:
+        return 0
+    seen.add(obj_id)
+    if isinstance(obj, dict):
+        size += sum(get_size(v, seen) for v in obj.values())
+        size += sum(get_size(k, seen) for k in obj.keys())
+    elif hasattr(obj, "__dict__"):
+        size += get_size(obj.__dict__, seen)
+    elif hasattr(obj, "__iter__") and not isinstance(obj, (str, bytes, bytearray)):
+        size += sum(get_size(i, seen) for i in obj)
+    return size
 
 
 with timing("importing module"):
@@ -33,9 +51,12 @@ sample = "/tmp/NeTEx_HTM__2020-10-12.xml"
 context = XmlContext()
 config = ParserConfig(fail_on_unknown_properties=False)
 
+
+from netex.models import PublicationDelivery  # noqa
+
 with timing("XmlContext warmup"):
     context.build_recursive(PublicationDelivery)
-    print(f"Context cache size: {len(context.cache)}")
+    print(f"Context cache size: {get_size(context.cache)}")
 
 parser = XmlParser(context=context, config=config, handler=LxmlEventHandler)
 with timing("Parse[LxmlEventHandler]"):
